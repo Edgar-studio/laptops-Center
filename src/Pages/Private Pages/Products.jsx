c9d0e237fd39a0ccf1b02fec18a4fbfb70aa7587
Edgar-studio <edgaravakimyan7@gmail.com>
2025-11-28 17:58:57 +0100

    da

--- src/Pages/Private Pages/Products.jsx
+++ src/Pages/Private Pages/Products.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import { fetchProducts } from "../../Toolkit/Slices/ProductSlice.js";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Products = () => {
     return <div>Products Component</div>;
